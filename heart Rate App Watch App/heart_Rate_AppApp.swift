c9d387d011df4a4642016aa2bdddc75d96fa36fa
Patrick Henry <patrickhenrymcsd@gmail.com>
2023-10-06 13:41:14 -0600

    Initial Commit

---
+++ heart Rate App Watch App/heart_Rate_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  heart_Rate_AppApp.swift
+//  heart Rate App Watch App
+//
+//  Created by Patrick Henry on 10/6/23.
+//
+
+import SwiftUI
+
+@main
+struct heart_Rate_App_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
